21671cd593c3f9ec4c778409d89d591b00d2a258
Sarah <sdeluna050@gmail.com>
2025-12-11 21:59:10 +0100

    feat(gestion_logs): Iteration 2 - Final code structure (switch and loop)

--- main.cpp
+++ main.cpp
@@ -1,32 +1,53 @@
-#include <iostream>     //La seule lib nécessaire pour écrire/lire
+/**
+* @file main.cpp
+ * @brief Application de gestion de logs. (Mission 3 - Iteration 2)
+ * This program displays a menu for interacting with different types of logs.
+ * It uses a do...while loop and a switch case for the user interface.
+ */
 
-using namespace std;   //On l'utilise pour ne pas devoir écrire "std::" devant cout et endl.
+#include <iostream>
+
+using namespace std;
 
 int main() {
-    //Afficher le titre de l'application
-    cout << "CNED-Gestion de logs" << endl;
-    //Afficher le menu
-    cout << "---MENU---" << endl;
-    cout << "1. Afficher log sudo" << endl;
-    cout << "2. Afficher et enregistrer log SSH" << endl;
-    cout << "3. Quitter" << endl;
+    int choixUtilisateur;
 
-    int choixUtilisateur; //création de la boite
+    //Start of the loop (DO).
+    // We open the brace here to say: “Do all this...”
+    do {
 
-    cout <<"Choisir une option : ";  //Utilisation de COUT pour demander a la personne de taper son choix
-    cin >> choixUtilisateur;        //Utilisation de CIN pour prendre ce qui est tapé et le mettre dans la boite
-    cout << "Vous avez tapé: " << choixUtilisateur << endl;
+        // 1. Menu display
+        cout << endl << "--- MENU ---" << endl;
+        cout << "1. Display sudo log" << endl;
+        cout << "2. Display and save SSH log" << endl;
+        cout << "3. Exit" << endl;
 
-    //On regarde ce que contient la boite
-    if (choixUtilisateur == 1) {
-        cout <<"Vous avez choisi l'option 1." << endl;
-    }
-    else if (choixUtilisateur == 2) {
-        cout <<"Vous avez choisi l'option 2." << endl;
-    }
-    else if (choixUtilisateur == 0) {
-        cout <<"Sortie du programme." << endl;
-    }
-    return 0; // Le programme s'est bien terminé
+        // 2. User request
+        cout << "Select an option : ";
+        cin >> choixUtilisateur;
 
+        // 3. Treatment of choice (SWITCH)
+        switch (choixUtilisateur) {
+            case 1:
+                cout << "You have selected option 1." << endl;
+                break; // Stop, we're leaving the switch
+
+            case 2:
+                cout << "You have chosen option 2." << endl;
+                break; //Stop, we're leaving the switch
+
+            case 3:
+                cout << "Sortie du programme..." << endl;
+                break; // Stop, we're leaving the switch
+
+            default: // Si ce n'est ni 1, ni 2, ni 3
+                cout << "Invalid choice." << endl;
+                break;
+        } // End of switch
+
+    } while (choixUtilisateur != 3);
+    // END OF LOOP (WHILE)
+    // “As long as the choice is different from 3, we go back to ‘do’.”
+
+    return 0;
 }
